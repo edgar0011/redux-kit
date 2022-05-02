769506c2bbf840824a7715bcc738a7b8cf43899a
Martin Weiser <martin.weiser@gmail.com>
2022-05-02 11:34:53 +0200

    feat: adds camelCase wrpper ove lodash-es/camelCase

--- src/core/utils/quarks.spec.js
+++ src/core/utils/quarks.spec.js
@@ -1,4 +1,4 @@
-import { capitalize, capitalize2 } from './quarks'
+import { capitalize, capitalize2, upperSnakeCase, camelCase } from './quarks'
 
 describe('capialize', () => {
   it('should capitalize words', () => {
@@ -9,5 +9,24 @@ describe('capialize', () => {
 describe('capitalize2', () => {
   it('should capitalize2 words', () => {
     expect(capitalize2('hello')).toEqual('Hello')
+  })
+})
+describe('upperSnakeCase', () => {
+  it('should upperSnakeCase words', () => {
+    expect(upperSnakeCase('hello world')).toEqual('HELLO_WORLD')
+  })
+})
+
+describe('camelCase', () => {
+  it('should camelCase words', () => {
+    expect(camelCase(['hello', 'world'])).toEqual('helloWorld')
+  })
+
+  it('should camelCase words', () => {
+    expect(camelCase(['', 'world'])).toEqual('world')
+  })
+
+  it('should camelCase words', () => {
+    expect(camelCase(['hi yes', 'no', 'world'])).toEqual('hiYesNoWorld')
   })
 })
--- src/core/utils/quarks.ts
+++ src/core/utils/quarks.ts
@@ -2,25 +2,30 @@ import {
   replace, head, toUpper, concat, tail, pipe,
 } from 'ramda'
 import snakeCase from 'lodash-es/snakeCase'
+import camelCaseLES from 'lodash-es/camelCase'
 
 export const DEFAULT_LOAD_NAME = 'load'
 export const DEFAULT_UPDATE_NAME = 'update'
 export const DEFAULT_REMOVE_NAME = 'remove'
 export const DEFAULT_ACTON_PAYLOAD_NAME = 'payload'
 
-export const capitalize = (string: string) => {
-  if (!string) {
+export const capitalize = (value: string) => {
+  if (!value) {
     return ''
   }
-  const firstLetter = head(string)
-  return replace(firstLetter, toUpper(firstLetter), string)
+  const firstLetter = head(value)
+  return replace(firstLetter, toUpper(firstLetter), value)
 }
 
-export const capitalize2 = (string: string) => (!string ? '' : concat(
-  toUpper(head(string)), tail(string),
+export const capitalize2 = (value: string) => (!value ? '' : concat(
+  toUpper(head(value)), tail(value),
 ))
 
 export const upperSnakeCase = pipe(
   snakeCase,
   toUpper,
 )
+
+type ValueString = string[] | string
+
+export const camelCase = (value: ValueString): string => camelCaseLES(Array.isArray(value) ? value.join(' ') : value)
